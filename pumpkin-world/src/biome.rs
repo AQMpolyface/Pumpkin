10cda7949d2b86228ab5509710eed8150547438f
polyface <jonas.morisoli@proton.me>
2024-12-09 22:35:18 +0100

    deleted picture

--- pumpkin-world/src/biome.rs
+++ pumpkin-world/src/biome.rs
@@ -1,3 +1,4 @@
+use enum_dispatch::enum_dispatch;
 use serde::{Deserialize, Serialize};
 // TODO make this work with the protocol
 // Send by the registry
@@ -70,16 +71,20 @@ pub enum Biome {
 pub enum BiomeSupplier {
     Debug(DebugBiomeSupplier),
 }
+
 #[enum_dispatch]
 pub trait BiomeSupplierImpl {
     fn biome(&self, x: i32, y: i32, z: i32, noise: &MultiNoiseSampler) -> Biome;
 }
+
 #[derive(Clone)]
 pub struct DebugBiomeSupplier {}
+
 impl BiomeSupplierImpl for DebugBiomeSupplier {
-    fn biome(&self, *x: i32, *y: i32, *z: i32, *noise: &MultiNoiseSampler) -> Biome {
+    fn biome(&self, _x: i32, _y: i32, _z: i32, _noise: &MultiNoiseSampler) -> Biome {
         Biome::Plains
     }
 }
+
 // TODO: Implement
 pub struct MultiNoiseSampler {}
